d2ab0267b5a65e324e17a6f02f7f22b2fe2d0b81
SharapovaMarina <sanitzkaja.mar@yandex.ru>
2024-08-19 16:45:59 +0300

    Add files via upload

---
+++ Организация программ и методы строк0.py
@@ -0,0 +1,12 @@
+my_string = input("Введите ваше имя: ")
+#current_date = 2024
+#date_of_birth = int(input("Введите год вашего рождения: "))
+#age = current_date - date_of_birth
+#print("Здравствуйте, ", my_string)
+#print("В этом году вам ", age, "лет")
+print("my_string".upper())
+print("my_string".lower())
+print("my string".replace("my string", "mystring"))
+name = "my_string"
+print(name[0])
+print(name[-1])
